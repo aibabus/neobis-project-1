045a4806b4691dfdd6256ae0a9cdccde7d74698e
aibabus <aibek.nazaraliev003005@gmail.com>
2023-09-07 21:07:54 +0600

    add login except of email

--- src/main/java/com/shop/ShopApplication/config/JwtAuthenticationFilter.java
+++ src/main/java/com/shop/ShopApplication/config/JwtAuthenticationFilter.java
@@ -27,15 +27,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
-        final String userEmail;
+        final String userLogin;
         if(authHeader == null || !authHeader.startsWith("Bearer ")){
             filterChain.doFilter(request, response);
             return;
         }
         jwt = authHeader.substring(7);
-        userEmail = jwtService.extractUsername(jwt);
-        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
-            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+        userLogin = jwtService.extractUsername(jwt);
+        if(userLogin != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userLogin);
             if(jwtService.isTokenValid(jwt, userDetails)){
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                         userDetails, null, userDetails.getAuthorities()
--- src/main/java/com/shop/ShopApplication/register/RegisterRequest.java
+++ src/main/java/com/shop/ShopApplication/register/RegisterRequest.java
@@ -13,5 +13,4 @@ public class RegisterRequest {
     private String login;
     private String email;
     private String password;
-
 }
